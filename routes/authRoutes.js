89b7257c0b30194aca9fab029790173aabc085b0
Fabio Medina <disenofabiomedina@gmail.com>
2023-05-22 18:29:38 -0500

    Change password login validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,11 +16,7 @@ router.post(
   '/',
   [
     check('email', 'El email es obligatorio o no es valido').isEmail(),
-    check(
-      'password',
-      'La contraseña debe tener más de 6 caracteres y por lo menos una mayúscula y un número')
-      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/
-    ),
+    check('password', 'La contraseña es obligatoria o no es válida').notEmpty(),
     fieldValidators
   ],
   loginUser
@@ -34,8 +30,7 @@ router.post(
     check(
       'password',
       'La contraseña debe tener más de 6 caracteres y por lo menos una mayúscula y un número')
-      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/
-    ),
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/),
     fieldValidators
   ],
   createUser,
